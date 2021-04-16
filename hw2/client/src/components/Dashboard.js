b894f68fdabbe54bd9577dbec80c88e55f2d9c2b
ktien231 <kyletien@seas.upenn.edu>
2021-04-15 22:22:11 -0400

    changed dashboard titles

--- hw2/client/src/components/Dashboard.js
+++ hw2/client/src/components/Dashboard.js
@@ -19,35 +19,6 @@ export default class Dashboard extends React.Component {
     this.showMovies = this.showMovies.bind(this);
   }
 
-  // React function that is called when the page load.
-  componentDidMount() {
-    // Send an HTTP request to the server.
-    fetch("http://localhost:8081/genres",
-      {
-        method: 'GET' // The type of HTTP request.
-      }).then(res => {
-        // Convert the response data to a JSON.
-        return res.json();
-      }, err => {
-        // Print the error if there is one.
-        console.log(err);
-      }).then(genreList => {
-        if (!genreList) return;
-        // Map each genreObj in genreList to an HTML element:
-        // A button which triggers the showMovies function for each genre.
-        let genreDivs = genreList.map((genreObj, i) =>
-          <GenreButton id={"button-" + genreObj.genre} onClick={() => this.showMovies(genreObj.genre)} genre={genreObj.genre} />
-        );
-        // Set the state of the genres list to the value returned by the HTTP response from the server.
-        this.setState({
-          genres: genreDivs
-        });
-      }, err => {
-        // Print the error if there is one.
-        console.log(err);
-      });
-  }
-
 
   /* ---- Q1b (Dashboard) ---- */
   /* Set this.state.movies to a list of <DashboardMovieRow />'s. */
@@ -82,7 +53,7 @@ export default class Dashboard extends React.Component {
         <br></br>
         <div className="container movies-container">
           <div className="jumbotron">
-            <div className="h5">Top Movies</div>
+            <div className="h5">Worst Covid Day Per State</div>
             <div className="genres-container">
               {this.state.genres}
             </div>
@@ -92,9 +63,9 @@ export default class Dashboard extends React.Component {
           <div className="jumbotron">
             <div className="movies-container">
               <div className="movies-header">
-                <div className="header-lg"><strong>Title</strong></div>
-                <div className="header"><strong>Rating</strong></div>
-                <div className="header"><strong>Vote Count</strong></div>
+                <div className="header"><strong>State</strong></div>
+                <div className="header"><strong>Date</strong></div>
+                <div className="header"><strong>Number of Cases</strong></div>
               </div>
               <div className="results-container" id="results">
                 {this.state.movies}
